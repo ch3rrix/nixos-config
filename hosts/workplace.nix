ce4bc8b366fdc5e45c2a1283d7f079c19d6cfa04
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-07-26 01:40:37 +0300

    haha

--- hosts/workplace.nix
+++ hosts/workplace.nix
@@ -23,6 +23,7 @@
 
 
   services.printing.enable = true;
+  networking.hostName = "workplace";
 
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
