021e40192b2d3d1726330fc0ac5463f4229cf4b8
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-07-28 01:22:39 +0300

    Catppuccin in alacritty

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -3,5 +3,15 @@
   programs.alacritty = {
     enable = true;
 
+    settings = {
+      colors ={
+        primary = {
+          background = "#1e1e2e";
+          foreground = "#cdd6f4";
+          dim_foreground = "cdd6f4";
+          bright_foreground = "#cdd6f4";
+        };
+      };
+    };
   };
 }
