d97270f00d990cc7c57cee3c8a3fa1ae69f77c65
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-08-03 18:56:51 +0300

    emacs 29 is mine

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -2,9 +2,11 @@
 {
   programs.emacs = {
     enable = true;
+    package = pkgs.emacs29;
   };
   services.emacs = {
     enable = true;
+    package = pkgs.emacs29;
     client.enable = true;
   };
 }
