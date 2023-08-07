fb1fd8d32e0c368041213e5c31ed0de01f3d7891
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-08-08 02:31:28 +0300

    trying to rewrite librewolf module

--- home/workplace.nix
+++ home/workplace.nix
@@ -39,6 +39,7 @@
             urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
             iconUpdateURL = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
+
             definedAliases = [ "@nw" ];
           };
         }; # search engines
