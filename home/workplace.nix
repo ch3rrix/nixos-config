785f404a6fb314f72ae4ca396c57699a0f3f96d5
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-08-08 01:29:10 +0300

    trying to rewrite librewolf module

--- home/workplace.nix
+++ home/workplace.nix
@@ -18,6 +18,37 @@
 
   programs.librewolf1 = {
     enable = true;
+    profiles = {
+      test = {
+        bookmarks = [
+          {
+            name = "wikipedia";
+            tags = [ "wiki" ];
+            keyword = "wiki";
+            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+          }
+          {
+            name = "kernel.org";
+            url = "https://www.kernel.org";
+          }
+          {
+            name = "Nix sites";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "homepage";
+                url = "https://nixos.org/";
+              }
+              {
+                name = "wiki";
+                tags = [ "wiki" "nix" ];
+                url = "https://nixos.wiki/";
+              }
+            ];
+          }
+        ];
+      };
+    };
   };
 
   home = {
