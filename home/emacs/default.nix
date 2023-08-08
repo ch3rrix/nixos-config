2fdb689d222e47ae7829b4ea36e3444b1ba5b550
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-08-09 01:12:29 +0300

    idk

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -1,11 +1,21 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs29;
+    extraConfig = ''
+    (require 'package)
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+    ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+    ;; and `package-pinned-packages`. Most users will not need or want to do this.
+    ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+    (package-initialize)
+    '';
   };
+
   services.emacs = {
     enable = true;
     package = pkgs.emacs29;
     client.enable = true;
+    startWithUserSession = true;
   };
 }
