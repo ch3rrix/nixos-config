868ae38eaba7e2144ac3605874fd9e0e411fd047
Ruslan Rakshinsky <ch3rrix@proton.me>
2023-08-17 19:08:15 +0300

    wallpapers???

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -1,8 +1,17 @@
+{ pkgs, ... }:
 {
   programs.fish = {
     enable = true;
     plugins = [
-
+      {
+      name = "nix-completions";
+      src = pkgs.fetchFromGitHub {
+        owner = "kidonng";
+        repo = "nix-completions.fish";
+        rev = "cd8a43bed96e0acc02228bc77502be8ba5fa0548";
+        sha256 = "sha256-spnLmde41qQt8uJZFwiH0igFuVqZ6SvkwdA9Kbe2yz8=";
+      };
+  }
     ];
     shellAliases = {
       ".." = "cd ..";
--- home/workplace.nix
+++ home/workplace.nix
@@ -13,6 +13,7 @@
     ./java
     ./emacs
     ./fonts
+    ./fish
   ];
 
   home = {
